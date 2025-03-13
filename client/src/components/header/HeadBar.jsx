1cf9bfde936e56da9b9b0f5e02a5feefcc794d47
Troshina Alesya <a.trowina@gmail.com>
2025-03-13 16:37:58 +0500

    [D-24] Header теперь имеет горизонтальное меню

--- client/src/components/header/HeadBar.jsx
+++ client/src/components/header/HeadBar.jsx
@@ -1,11 +1,23 @@
 import React, { useContext } from 'react';
-import { Layout } from 'antd';
+import { Layout, Menu } from 'antd';
 import { GlobalOutlined } from '@ant-design/icons';
-import { HOME_ROUTE, LOGIN_ROUTE } from '../../utils/consts';
 import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 import { Context } from '../..';
 import './HeadBar.css';
 import { observer } from 'mobx-react-lite';
+import {
+	BarsOutlined,
+	HomeOutlined,
+	SettingOutlined,
+	UserOutlined,
+} from '@ant-design/icons';
+import {
+	COURSE_ROUTE,
+	HOME_ROUTE,
+	LOGIN_ROUTE,
+	PROFILE_ROUTE,
+	REGISTRATION_ROUTE,
+} from '../../utils/consts';
 
 const { Header } = Layout;
 
@@ -32,6 +44,25 @@ const HeadBar = observer(() => {
 
 	console.log('выполнен вход пользователя в систему?', user.isAuth);
 
+	const getItem = (key, label, icon, href) => {
+		return {
+			key,
+			label,
+			icon,
+			href,
+		};
+	};
+
+	const items = [
+		// getItem('0', 'Профиль', <UserOutlined />, PROFILE_ROUTE),
+		getItem('0', 'Курс', <BarsOutlined />, COURSE_ROUTE),
+		getItem('1', 'Личный кабинет', <UserOutlined />, LOGIN_ROUTE),
+	];
+
+	const handlerClick = e => {
+		return history.push(items[e.key].href);
+	};
+
 	return (
 		<Header className='header' style={user.isAuth ? styleAuth : styleUser}>
 			<div className='h-left'>
@@ -40,11 +71,20 @@ const HeadBar = observer(() => {
 				</h2>
 			</div>
 			<div className='demo-logo h-right'>
+				<Menu
+				className='menu-horizontal-my'
+					theme='light'
+					mode='horizontal'
+					items={items}
+					onClick={e => handlerClick(e)}
+				/>
+			</div>
+			{/* <div className='demo-logo h-right'>
 				<p className='login-text' onClick={handlePageClick} title='Войти'>
 					Личный кабинет
 				</p>
-				{/* <GlobalOutlined onClick={handlePageClick} title='Войти'/> */}
-			</div>
+				<GlobalOutlined onClick={handlePageClick} title='Войти'/>
+			</div> */}
 		</Header>
 	);
 });
