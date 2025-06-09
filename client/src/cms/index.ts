dc8800e3d38188a26099a7bd4766ab5ebb16347b
Troshina Alesya <a.trowina@gmail.com>
2025-06-09 10:19:18 +0500

    [Q-1] Блок тестирования

--- client/src/cms/index.ts
+++ client/src/cms/index.ts
@@ -1,3 +1,4 @@
 export { default as modul } from './modul.json';
 export { default as fact } from './fact.json';
 export { default as images } from './images.json';
+export { default as quiz } from './quiz.json';
